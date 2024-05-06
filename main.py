1f286698e3d960122e8c93d7b445dbe2905a242d
Minabishay123 <minabishay@hotmail.com>
2024-05-06 16:38:37 +1000

    added class for finance tracker and initialised whilst adding expenses, budgets, savings and savings goal

---
+++ main.py
@@ -0,0 +1,19 @@
+class Personal_Finance_Tracker:
+    """
+    A class to represent a personal finance tracker.
+
+    Attributes:
+    - expenses (dict): A dictionary to store expenses, where keys are expense categories and values are amounts.
+    - budgets (dict): A dictionary to store budgets, where keys are budget categories and values are amounts.
+    - savings (float): The total amount of savings.
+    - savings_goal (float): The savings goal to be achieved.
+    """
+
+    def __init__(self):
+        """
+        Initializes a Personal_Finance_Tracker object with empty expenses, budgets, savings, and savings_goal.
+        """
+        self.expenses = {}
+        self.budgets = {}
+        self.savings = 0
+        self.savings_goal = 0
